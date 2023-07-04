34d7ee1ee6c8fab9b804f8f18538d269ae19114e
hinano0610 <136045154+hinano0610@users.noreply.github.com>
2023-07-04 11:26:39 +0900

    title changed

---
+++ resources/views/edit.blade.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>新規登録</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f2f2f2;
+            margin: 0;
+            padding: 0;
+        }
+
+        .container {
+            max-width: 600px;
+            margin: 0 auto;
+            padding: 20px;
+            background-color: #fff;
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+            border-radius: 4px;
+            margin-top: 50px;
+        }
+
+        .form-group {
+            margin-bottom: 20px;
+        }
+
+        label {
+            display: block;
+            font-weight: bold;
+            margin-bottom: 5px;
+        }
+
+        input[type="text"],
+        textarea {
+            width: 100%;
+            padding: 10px;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            resize: vertical;
+        }
+
+        button {
+            padding: 10px 20px;
+            background-color: #4CAF50;
+            color: #fff;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+        }
+
+        button:hover {
+            background-color: #45a049;
+        }
+
+        .error-message {
+            color: red;
+            margin-top: 5px;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1>新規登録</h1>
+
+        <form method="POST" action="/todos">
+            <div class="form-group">
+                <label for="title">タイトル:</label>
+                <input type="text" name="title" id="title" required>
+                @error('title')
+                    <div class="error-message">{{ $message }}</div>
+                @enderror
+            </div>
+
+            <div class="form-group">
+                <label for="content">投稿内容:</label>
+                <textarea name="content" id="content" required></textarea>
+                @error('content')
+                    <div class="error-message">{{ $message }}</div>
+                @enderror
+            </div>
+
+            <div class="form-group">
+                <label for="image">画像:</label>
+                <input type="file" name="image" id="image">
+                @error('image')
+                    <div class="error-message">{{ $message }}</div>
+                @enderror
+            </div>
+
+            <button type="submit">投稿する</button>
+        </form>
+    </div>
+</body>
+</html>
